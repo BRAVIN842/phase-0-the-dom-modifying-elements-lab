ca4b4005599d41d6652c3fece9eb588b464e75b2
Bravin <onsasebravin853@gmail.com>
2023-09-29 16:14:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Bravin is the champion";
+
+
+const mainElement = document.getElementById("main");
+
+
+if (mainElement) {
+  mainElement.remove();
+}
+
+const existingHeader = document.getElementById("main");
+
+if (existingHeader) {
+  existingHeader.replaceWith(newHeader);
+}
